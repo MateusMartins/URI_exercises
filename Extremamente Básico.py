37318ad625c7cbe57ff7d7757463b6753542b2fb
Mateus Martins <mateusmferreira@live.com>
2018-09-23 16:51:45 -0300

    Primeiro exercicio e exmplo

---
+++ Extremamente Básico.py
@@ -0,0 +1,8 @@
+#Efetua a leitura da variável inteira
+A = int(input())
+#Efetua a leitura da variável inteira
+B = int(input())
+#Efetua a soma dos valores A e B
+X = (A+B)
+#Exibe o Valor da soma
+print("X =", X)
---
+++ exemplo.py
@@ -0,0 +1,7 @@
+#Exemplo de Soma
+a = int(input("Valor 1: "))
+b = int(input("Valor 2: "))
+
+X = a + b
+
+print("X = %i" % X)
