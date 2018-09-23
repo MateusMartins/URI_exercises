693adf607763774f918b2e656454cca77a0d8055
Mateus Martins <mateusmferreira@live.com>
2018-09-23 18:39:42 -0300

    exercicios

---
+++ Salário.py
@@ -0,0 +1,12 @@
+#Valor inteiro A
+Funcionarios = int(input())
+#Efetua flutuante B
+Horas = float(input())
+#Efetua flutuante C
+ValorHora = float(input())
+#Efetua o cálculo do salário
+Salario = (Horas  * ValorHora)
+#Exibe a quantidade de funcionários
+print("NUMBER = %i" %Funcionarios)
+#Exibe o valor do salário
+print("SALARY = U$ %0.2f" %Salario)
