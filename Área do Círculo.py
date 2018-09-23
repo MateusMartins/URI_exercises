4bfefbb9bfcb8517f563587b5a9cad5a2194b17c
Mateus Martins <mateusmferreira@live.com>
2018-09-23 17:04:02 -0300

    area do circulo

---
+++ Área do Círculo.py
@@ -0,0 +1,8 @@
+#Valor do PI atribuido a variável n
+n = 3.14159
+#Efetua a leitura da variável float
+raio = float(input())
+#Efetua o cálculo da área do círculo
+area = n*(raio**2)
+#Exibe o valor do cálculo
+print("A=%0.4f" %area)
