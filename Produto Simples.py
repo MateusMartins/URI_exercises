ef0a9f1fda03220512a5182654dad504ae139a72
Mateus Martins <mateusmferreira@live.com>
2018-09-23 17:16:52 -0300

    exercicios novo

---
+++ Produto Simples.py
@@ -0,0 +1,8 @@
+#Efetua a leitura da variável inteira
+A = int(input())
+#Efetua a leitura da variável inteira
+B = int(input())
+#Efetua o produto dos valores A e B
+PROD = (A*B)
+#Exibe o Valor do produto
+print("PROD = %i" %PROD)
